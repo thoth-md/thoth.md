bcdf528eff9094f8eb0c236800346b98c657223e
Bryan Jenks <63628719+bryansjenks@users.noreply.github.com>
2026-04-27 23:40:43 -0400

    Comment out Yarn setup in setup.sh
    Comment out Yarn setup commands in setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,9 +3,9 @@ set -e
 
 # --- Yarn setup ---
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
-sudo corepack enable
-yarn config set --home enableTelemetry 0
-yarn dlx @yarnpkg/sdks vscode
+#sudo corepack enable
+#yarn config set --home enableTelemetry 0
+#yarn dlx @yarnpkg/sdks vscode
 
 # --- AI Agents ---
 npm install -g @anthropic-ai/claude-code
